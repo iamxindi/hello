0c0ff59a5d9d6925ef8adbe57774731557818184
pansyjia <siyujia@umich.edu>
2018-01-25 14:37:01 -0500

    change

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 # 123
 print("my name is siyu")
 <<<<<<< HEAD
-print("my name is also xx")
+print("my name is also pansy")
 =======
 print("my name is also xindi")
 >>>>>>> bd1381a9a4734c2cc530d3db0411356a6cc8ac37
